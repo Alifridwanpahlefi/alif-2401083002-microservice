5f2d4d6b51c61858fa525f1491ff7acd52072bad
Alif Ridwan Pahlefi <alifridwan364@gmail.com>
2026-04-08 06:59:04 +0700

    Add files via upload

---
+++ consumer/src/test/java/com/alif/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alif.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
