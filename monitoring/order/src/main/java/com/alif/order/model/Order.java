7f417b6642c5e6d6af47683d2fe4c5facd7a1304
Alif Ridwan Pahlefi <alifridwan364@gmail.com>
2026-07-25 17:47:05 +0700

    Add files via upload

---
+++ monitoring/order/src/main/java/com/alif/order/model/Order.java
@@ -0,0 +1,106 @@
+package com.alif.order.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long produk_id;
+    private Long id_pelanggan;
+    private int jumlah;
+    private double harga;
+    private double total;
+    private String tanggal;
+    private String status;
+    private String createdBy;
+    private String role;
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getId_produk() {
+        return produk_id;
+    }
+
+    public void setId_produk(Long produk_id) {
+        this.produk_id = produk_id;
+    }
+
+    public Long getId_pelanggan() {
+        return id_pelanggan;
+    }
+
+    public void setId_pelanggan(Long id_pelanggan) {
+        this.id_pelanggan = id_pelanggan;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", produk_id=" + produk_id +
+                ", id_pelanggan=" + id_pelanggan +
+                ", harga=" + harga +
+                ", jumlah=" + jumlah +
+                ", total=" + total +
+                ", tanggal='" + tanggal + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
---
+++ monitoring/order/src/main/java/com/alif/order/service/OrderService.java
@@ -0,0 +1,90 @@
+package com.alif.order.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.alif.order.model.Order;
+import com.alif.order.repository.OrderRepository;
+import com.alif.order.vo.Produk;
+import com.alif.order.vo.ResponseTemplate;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order creatOrder(Order order) {
+         if (order.getStatus() == null) {
+            order.setStatus("NEW");
+        }
+
+        Order savedOrder = orderRepository.save(order);
+
+        // 🔥 KIRIM KE RABBITMQ
+        rabbitTemplate.convertAndSend("myQueue", savedOrder.toString());
+
+        System.out.println("📤 Order dikirim ke RabbitMQ");
+
+        return savedOrder;
+    }
+
+    @Transactional
+    public void update(Long orderId, Integer jumlah, String tanggal, String status) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalStateException("Order tidak ada"));
+        if (jumlah != null) {
+            order.setJumlah(jumlah);
+        }
+        if (tanggal != null && tanggal.length() > 0
+            && !Objects.equals(order.getTanggal(), tanggal)) {
+                order.setTanggal(tanggal);
+            }
+
+        if (status != null && status.length() > 0) {
+            order.setStatus(status);
+        }
+    }
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public List<ResponseTemplate> getOrderWithProdukById(Long id) {
+        List<ResponseTemplate> responseList = new ArrayList<>();
+        Order order = getOrderById(id);
+        ServiceInstance serviceInstance = discoveryClient.getInstances("PRODUK").get(0);
+        String url = serviceInstance.getUri().toString() + "/api/produk/" + order.getId_produk();
+        Produk produk = restTemplate.getForObject(url, Produk.class);
+        ResponseTemplate vo = new ResponseTemplate();
+        vo.setOrder(order);
+        vo.setProduk(produk);
+        responseList.add(vo);
+        return responseList;
+    }
+
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
+}
